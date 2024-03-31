b43f47b182ee6c6bfc3de3e46a78a12f95aaef31
illiazubariev <zubarievillia@gmail.com>
2024-03-31 16:38:19 +0200

    add test pages

--- itemShop/myapp/views.py
+++ itemShop/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+
+def any(request):
+    return HttpResponse('any texts')
+
+def index(request):
+    my_num = 33
+    my_str = 'some string'
+    return render(request, 'index.html', {
+        'my_num': my_num, 'my_str': my_str
+        })
