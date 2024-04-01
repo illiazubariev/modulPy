07b81aee3d1b6005d0e1f7db87b9922925cd6a9e
illiazubariev <zubarievillia@gmail.com>
2024-04-01 10:07:09 +0200

    add test models product and wallet and migrate

--- itemShop/myapp/models.py
+++ itemShop/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=5, decimal_places=2)
