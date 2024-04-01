aa7c92f258488cabcd983f3adb08055f9dd8d4dd
illiazubariev <zubarievillia@gmail.com>
2024-04-01 14:45:21 +0200

    add and fix models

--- itemShop/myapp/models.py
+++ itemShop/myapp/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+from django.utils import timezone
 
 class Wallet(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    balance = models.DecimalField(max_digits=5, decimal_places=2)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+User.wallet = property(lambda u: Wallet.objects.get_or_create(user=u)[0])
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+class Return(models.Model):
+    purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
